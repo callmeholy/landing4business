ffa26409890d3d980212fef04578fdc1c2cc50ab
buzha <buzhaev-va@yandex.ru>
2026-02-20 22:20:53 +0300

    v0.4

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import phonenumbers
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', )
 def main_page():
     return render_template('index.html')
+
+
+@app.route('/send_lead', methods=['POST'])
+def send_lead():
+    name = request.form.get('userName')
+    tel = request.form.get('userPhone')
+    parsed_tel = phonenumbers.parse(tel)
+    if phonenumbers.is_valid_number(parsed_tel):
 
 
 if __name__ == '__main__':
---
+++ bot.py
@@ -0,0 +1,17 @@
+import telebot
+from tokens import bot_token, user_id
+
+
+bot = telebot.TeleBot(bot_token)
+
+
+def send_notification(name, number):
+    try:
+        bot.send_message(user_id, f'Вам пришла новая заявка:\nИмя: {name}\nТелефон: {number}')
+    except Exception as e:
+        print(f'Ошибка при отправке: {e}')
+
+
+
+
+
