f04d564ccd5ae174eb39263817235b207861931a
m4pkep <mrmarknumone@gmail.com>
2026-02-21 00:04:39 +0300

    v0.6m4

--- bot.py
+++ bot.py
@@ -5,13 +5,11 @@ from tokens import bot_token, user_id
 bot = telebot.TeleBot(bot_token)
 
 
+
+
 def send_notification(name, number):
     try:
         bot.send_message(user_id, f'Вам пришла новая заявка:\nИмя: {name}\nТелефон: {number}')
     except Exception as e:
         print(f'Ошибка при отправке: {e}')
-
-
-
-
 
