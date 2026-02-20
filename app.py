8ec324b8f9f612277d758b5633452409287f5949
buzha <buzhaev-va@yandex.ru>
2026-02-20 22:45:28 +0300

    v0.5

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template, request
 import phonenumbers
+from bot import send_notification
+
 app = Flask(__name__)
 
 
-@app.route('/', )
+@app.route('/', methods=['POST', 'GET'])
 def main_page():
+    if request.method == 'POST':
+        name = request.form.get('userName')
+        phone_number = request.form.get('userPhone')
+        send_notification(name, phone_number)
     return render_template('index.html')
-
-
-@app.route('/send_lead', methods=['POST'])
-def send_lead():
-    name = request.form.get('userName')
-    tel = request.form.get('userPhone')
-    parsed_tel = phonenumbers.parse(tel)
-    if phonenumbers.is_valid_number(parsed_tel):
 
 
 if __name__ == '__main__':
